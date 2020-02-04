92a5f3b08cfe60e3eb0330248fd0c037420f06c4
Ramazan Sakin <ramazansakin63@gmail.com>
2020-02-04 23:04:15 +0300

    project base impl via spring initializer

---
+++ src/test/java/com/sakinr/airportreservationsystem/AirportReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakinr.airportreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
