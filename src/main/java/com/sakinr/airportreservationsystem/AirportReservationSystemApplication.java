9a4b2ab67868dd3418770bb3e94ea51e53a31d33
r84160880 <ramazan.sakin@huawei.com>
2020-07-11 08:13:17 +0300

    Added Bean to unregister exception filter

--- src/main/java/com/sakinr/airportreservationsystem/AirportReservationSystemApplication.java
+++ src/main/java/com/sakinr/airportreservationsystem/AirportReservationSystemApplication.java
@@ -1,13 +1,27 @@
 package com.sakinr.airportreservationsystem;
 
+import com.sakinr.airportreservationsystem.filter.ExceptionHandlerFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AirportReservationSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AirportReservationSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AirportReservationSystemApplication.class, args);
+    }
+
+    // unregister any filter component
+    @Bean
+    public FilterRegistrationBean<ExceptionHandlerFilter> registration(ExceptionHandlerFilter filter) {
+    	// exception management handling manageable via both ExceptionHandlerFilter
+		// and GenericExceptionHandler interceptor, we can choose any of them
+		// I choose to stop filter
+        FilterRegistrationBean<ExceptionHandlerFilter> registration = new FilterRegistrationBean<>(filter);
+        registration.setEnabled(false);
+        return registration;
+    }
 
 }
