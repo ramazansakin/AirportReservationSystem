e07dea37214cbd403be2f3f4e357b58343efa959
Ramazan Sakin <ramazansakin63@gmail.com>
2020-07-12 12:31:30 +0300

    Adding some sample more complex queries via JPQL on TicketRepository

--- src/main/java/com/sakinr/airportreservationsystem/repository/AirportCompanyRepository.java
+++ src/main/java/com/sakinr/airportreservationsystem/repository/AirportCompanyRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AirportCompanyRepository extends JpaRepository<AirportCompany, Integer> {
+
+    AirportCompany getByName(String name);
 }
--- src/main/java/com/sakinr/airportreservationsystem/repository/TicketRepository.java
+++ src/main/java/com/sakinr/airportreservationsystem/repository/TicketRepository.java
@@ -1,16 +1,35 @@
 package com.sakinr.airportreservationsystem.repository;
 
 import com.sakinr.airportreservationsystem.entity.Ticket;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
 
-    Ticket getTicketsByPassengerId(Integer passengerId);
+    // JPA -Hibernate self-created query apis
+    List<Ticket> getTicketsByPassengerId(Integer passengerId);
 
-    List<Ticket> getTickersByFlightId(Integer flightId);
+    List<Ticket> getTicketsByFlightId(Integer flightId);
+
+    List<Ticket> getAllByFlight_Departure_dateBetween(Date startDate, Date endDate);
+
+    //JPQL - Java Persistence Query Language
+    @Query(
+            value = "SELECT * FROM ticket t where   ORDER BY id",
+            countQuery = "SELECT count(*) FROM Users",
+            nativeQuery = true)
+    Page<Ticket> getAllByFlightIdPagination(
+            @Param("flightId")Integer flightId,
+            Pageable pageable);
+
+
 
 }
