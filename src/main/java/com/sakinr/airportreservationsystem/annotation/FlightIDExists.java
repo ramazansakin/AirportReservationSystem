a8bb4525f5c64607bd1af484f4aa19955bd6329c
Ramazan Sakin <ramazansakin63@gmail.com>
2020-07-13 19:42:03 +0300

    Custom validation annotation added for FlightService

---
+++ src/main/java/com/sakinr/airportreservationsystem/annotation/FlightIDExists.java
@@ -0,0 +1,20 @@
+package com.sakinr.airportreservationsystem.annotation;
+
+import com.sakinr.airportreservationsystem.annotation.validator.FlightIDExistingValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Constraint(validatedBy = FlightIDExistingValidator.class)
+
+public @interface FlightIDExists {
+    String message() default "{validation.messages.flightidexists}";
+    
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/sakinr/airportreservationsystem/annotation/validator/FlightIDExistingValidator.java
@@ -0,0 +1,27 @@
+package com.sakinr.airportreservationsystem.annotation.validator;
+
+import com.sakinr.airportreservationsystem.annotation.FlightIDExists;
+import com.sakinr.airportreservationsystem.entity.Flight;
+import com.sakinr.airportreservationsystem.exception.NotFoundException;
+import com.sakinr.airportreservationsystem.service.FlightService;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+@RequiredArgsConstructor
+public class FlightIDExistingValidator implements ConstraintValidator<FlightIDExists, Long> {
+
+    private final FlightService flightService;
+
+    @Override
+    public boolean isValid(Long id, ConstraintValidatorContext constraintValidatorContext) {
+        // if not exists, it returns not found exception
+        try {
+            flightService.getFlight(id.intValue());
+        } catch (NotFoundException e) {
+            return true;
+        }
+        return false;
+    }
+}
