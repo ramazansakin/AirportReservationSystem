05e5fe7aba57387b292a36381d56f5707a567009
Ramazan Sakin <ramazansakin63@gmail.com>
2020-07-13 11:36:38 +0300

    File Operations controller added - upload a file via name - download a file via name not: you can change up-down place wherever you want

---
+++ src/main/java/com/sakinr/airportreservationsystem/fileops/controller/FileOpsController.java
@@ -0,0 +1,48 @@
+package com.sakinr.airportreservationsystem.fileops.controller;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/file-ops")
+public class FileOpsController {
+
+    @GetMapping(value = "/download")
+    public ResponseEntity<Object> downloadFile(@RequestParam("filename") String filename) throws IOException {
+        String filePath = "/var/tmp/" + filename;
+        File file = new File(filePath);
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        return ResponseEntity.ok().headers(headers).contentLength(file.length()).contentType(
+                MediaType.parseMediaType("application/txt")).body(resource);
+    }
+
+    @PostMapping(
+            value = "/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+
+        File convertFile = new File("/var/tmp/" + file.getOriginalFilename());
+        convertFile.createNewFile();
+        FileOutputStream fout = new FileOutputStream(convertFile);
+        fout.write(file.getBytes());
+        fout.close();
+        return "File is upload successfully";
+    }
+
+}
