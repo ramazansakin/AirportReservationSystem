ea3f78dfbcbd0e600f9ec35b71df599ff54df786
Ramazan Sakin <ramazansakin63@gmail.com>
2020-07-14 16:26:36 +0300

    new custom date validation annotation added

---
+++ src/main/java/com/sakinr/airportreservationsystem/annotation/CustomDateConstraint.java
@@ -0,0 +1,19 @@
+package com.sakinr.airportreservationsystem.annotation;
+
+import com.sakinr.airportreservationsystem.annotation.validator.CustomDateValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = CustomDateValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CustomDateConstraint {
+    String message() default "Invalid date format";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/sakinr/airportreservationsystem/annotation/validator/CustomDateValidator.java
@@ -0,0 +1,34 @@
+package com.sakinr.airportreservationsystem.annotation.validator;
+
+import com.sakinr.airportreservationsystem.annotation.CustomDateConstraint;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class CustomDateValidator implements
+        ConstraintValidator<CustomDateConstraint, LocalDate> {
+
+    private static final String DATE_PATTERN = "MM/dd/yyyy";
+
+    @Override
+    public void initialize(CustomDateConstraint customDate) {
+    }
+
+    @Override
+    public boolean isValid(LocalDate customDateField,
+                           ConstraintValidatorContext cxt) {
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_PATTERN);
+        try {
+            sdf.setLenient(false);
+            Date d = sdf.parse(String.valueOf(customDateField));
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+
+}
