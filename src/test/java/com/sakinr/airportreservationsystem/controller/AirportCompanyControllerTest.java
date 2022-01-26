6cfbe56096e8aaa23a08c5b61d6be10d5bf91774
Ramazan Sakin <ramazansakin63@gmail.com>
2022-01-26 06:20:51 +0300

    feat: some of AirportCompany Controller integration tests init

---
+++ src/test/java/com/sakinr/airportreservationsystem/controller/AirportCompanyControllerTest.java
@@ -0,0 +1,28 @@
+package com.sakinr.airportreservationsystem.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AirportCompanyControllerTest {
+
+    @Test
+    void welcome() {
+    }
+
+    @Test
+    void getAllAirportCompanies() {
+    }
+
+    @Test
+    void getAirportCompany() {
+    }
+
+    @Test
+    void saveAirportCompany() {
+    }
+
+    @Test
+    void cancelTicket() {
+    }
+}
