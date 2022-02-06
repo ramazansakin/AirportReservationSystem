fda0e8bf6dcb88fa4247897ef5ef4ca65dabdd03
Ramazan Sakin <ramazansakin63@gmail.com>
2022-02-06 09:40:51 +0300

    feat: clean up

--- src/main/java/com/sakinr/airportreservationsystem/repository/UserRepository.java
+++ src/main/java/com/sakinr/airportreservationsystem/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.sakinr.airportreservationsystem.repository;
 
 import com.sakinr.airportreservationsystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     boolean existsByUsername(String username);
