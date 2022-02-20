8e4d378d93709ea0e5c63a6bd604fbad21542c10
Ramazan Sakin <ramazansakin63@gmail.com>
2022-02-20 07:13:04 +0300

    feat: new config to disable security side with dev profile

--- src/main/java/com/sakinr/airportreservationsystem/security/WebSecurityConfig.java
+++ src/main/java/com/sakinr/airportreservationsystem/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.sakinr.airportreservationsystem.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Profile("test")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
