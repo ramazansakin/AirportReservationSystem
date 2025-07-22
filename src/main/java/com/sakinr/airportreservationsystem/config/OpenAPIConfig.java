b75b99d302ce17d883acebc49da634f5d10ecd2d
rsakin <ramazansakin63@gmail.com>
2025-07-22 05:44:01 +0300

    feat(JDK8 to  JDK21 migration) : Swagger migrated as OpenAPI

---
+++ src/main/java/com/sakinr/airportreservationsystem/config/OpenAPIConfig.java
@@ -0,0 +1,35 @@
+package com.sakinr.airportreservationsystem.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .info(new Info()
+                        .title("Airport Reservation System API")
+                        .description("API documentation for Airport Reservation System")
+                        .version("1.0")
+                );
+    }
+}
